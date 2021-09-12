608ab1dc1d0db38232b84b97976300cec190c162
Zaki Rucker <zakirucker@mac.com>
2021-09-12 11:56:57 -1000

    sanitized directory and .ignore

---
+++ 01_hello/ragz_hello.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Date   : 2021-09-06
+Purpose: Greet the user"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """parse the arguments"""
+    parser = argparse.ArgumentParser(
+        description='Greet the user',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-n', '--name',
+                        metavar='name', default='World',
+                        help='Name to greet')
+    return parser.parse_args()
+
+
+# -------------------------------------------------
+def main():
+    """where the magic happens"""
+    args = get_args()
+
+    print('Hello, ' + args.name + '!')
+
+
+if __name__ == '__main__':
+    main()
---
+++ 02_crowsnest/ragz_crowsnest.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Date   : 2021-09-06
+Purpose: learning to work with strings
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Crow\'s Nest -- choose the correct article',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('word',
+                        metavar='word',
+                        help='A word')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def get_article(user_input):
+    """Determine which article to use"""
+
+#    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    if user_input[0] in 'aeiouAEIOU':
+        solution = "an"
+    else:
+        solution = "a"
+
+    return solution
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    word = args.word
+
+    article = get_article(word)
+
+    print("Ahoy, Captain, {} {} off the larboard bow!".format(article, word))
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
